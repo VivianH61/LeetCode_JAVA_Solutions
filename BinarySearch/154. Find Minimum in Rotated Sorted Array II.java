dc34dfc26e78ba9eda2bbb35fde900f0d51d0301
Lanxin HU <scylh1@Nottingham.edu.cn>
2022-05-23 00:20:29 -0400

    binary search

---
+++ BinarySearch/154. Find Minimum in Rotated Sorted Array II.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            if (nums[l] < nums[r]) return nums[l];
+            int mid = (l + r) / 2;
+            if (nums[l] > nums[mid]) {
+                r = mid;
+            } else if (nums[l] < nums[mid]) {
+                l = mid + 1;
+            } else {
+                // skip duplicate element
+                l ++;
+            }
+        }
+        return nums[l];
+    }
+}
